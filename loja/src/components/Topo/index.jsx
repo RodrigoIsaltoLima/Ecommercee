082e89b004c87f012f34af7ac7181b52215c4a0a
RodrigoIsaltoLima <rodrigo.i.lima@outlook.com>
2026-03-17 08:34:23 -0300

    visual alterado

--- loja/src/components/Topo/index.jsx
+++ loja/src/components/Topo/index.jsx
@@ -4,7 +4,9 @@ export default function Topo() {
     return (
         <header>
             <div className='anuncio'>
-                <p>Tudo em 10x sem juros! </p>
+                <div className='anuncio-conteudo'>
+                    <p>Tudo em 10x sem juros! </p>
+                </div>
             </div>
 
             <div className='topo'>
